970392fdd44368fc874843da84cdb8cb9af0c36d
Nivi0284 <nivijaiswal45@gmail.com>
2024-03-23 16:50:28 +0530

    first commit

---
+++ src/main/java/com/backendAssignment/GyanGrove/GyanGroveApplication.java
@@ -0,0 +1,13 @@
+package com.backendAssignment.GyanGrove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyanGroveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyanGroveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/backendAssignment/GyanGrove/GyanGroveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendAssignment.GyanGrove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyanGroveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
